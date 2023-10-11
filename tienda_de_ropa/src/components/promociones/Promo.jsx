3556e718ba82e92740c0fb7e288b73de35b54ed7
Busferis <busferis7@gmail.com>
2023-10-11 19:29:40 -0300

    Actualizacion

---
+++ tienda_de_ropa/src/components/promociones/Promo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Promo() {
+  return (
+    <div className='sliderPromo'>
+        <Buscador />
+        <TrendPromo />
+    </div>
+  )
+}
+
+export default Promo
---
+++ tienda_de_ropa/src/components/promociones/TrendPromo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrendPromo = () => {
+  return (
+    <div>TrendPromo</div>
+  )
+}
+
+export default TrendPromo
