6c2ff46f755faff0883f7babd180d9e2436e79e7
Busferis <busferis7@gmail.com>
2023-11-15 20:58:43 -0300

    Actualizacion

--- tienda_de_ropa/src/views/Categories.jsx
+++ tienda_de_ropa/src/views/Categories.jsx
@@ -3,30 +3,34 @@ import '../components/categories.css'
 
 function Categories() {
   return (
-    <>
+    <div className='categories'>
       <div className="title">
-        <div>Categories</div>
+        <h3>Categories</h3>
         <a href="#">Show All</a>
       </div>
       <div className="bloques">
         <div className="bloque">
+          <span className="material-icons-outlined">face_4</span>
           <div className="categorias">
             Woman
           </div>
           <div className="categorias2">
             (728 items)
           </div>
+          <span className="material-icons-outlined">arrow_forward_ios</span>
         </div>
         <div className="bloque">
+          <span className="material-icons-outlined">face</span>
           <div className="categorias">
             Man
           </div>
           <div className="categorias2">
             (536 items)
           </div>
+          <span className="material-icons-outlined">arrow_forward_ios</span>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
