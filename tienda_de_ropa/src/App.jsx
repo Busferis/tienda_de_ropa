b520f536c27de0fb06cbce75e1655802774d9154
Busferis <busferis7@gmail.com>
2023-11-30 16:47:47 -0300

    Actualizacion

--- tienda_de_ropa/src/App.jsx
+++ tienda_de_ropa/src/App.jsx
@@ -1,7 +1,7 @@
 // import './App.css'
 import Landing from "./views/principal/Landing"
 import { BrowserRouter, Outlet } from "react-router-dom";
-import {Link, Route, Routes} from 'react-router-dom'
+import {Link, Route, Routes} from "react-router-dom";
 import Producto from "./views/producto/Producto";
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <Routes>
             <Route path="/tienda_de_ropa/Producto" element={<Producto />} />
             <Route index path="/tienda_de_ropa" element={<Landing />} />
+            <Route path="/tienda_de_ropa" element={<Producto />} />
       </Routes>
       </BrowserRouter>
     </>
--- tienda_de_ropa/src/views/producto/Producto.jsx
+++ tienda_de_ropa/src/views/producto/Producto.jsx
@@ -1,28 +1,42 @@
 import React from "react";
 import { ReactDOM } from "react";
 import '../../components/producto.css';
+import ropa from '../../data/pilcha.json';
+import { Link } from "react-router-dom"
 
 function Producto() {
     return(
         <>
+        <div className="barra2">
+            <Link to='/tienda_de_ropa'>
+                <span className="material-icons-outlined icono">arrow_back</span>
+            </Link>
+            <span className="material-icons-outlined icono">shopping_cart</span>
+        </div>
         <div className="promo_2">
-            <div className="sliderPromo2">
-                <div className="portada"></div>
-            </div>
-            <div className="tresPuntos">
-                <div className="puntito"></div>
-                <div className="puntoGrande2"></div>
-                <div className="puntito"></div>
+            <div className="portada">
+                <div className="tresPuntos">
+                    <div className="puntito"></div>
+                    <div className="puntoGrande2"></div>
+                    <div className="puntito"></div>
+                </div>
+                <img src={ropa[1].url} alt="" />
             </div>
         </div>
         <div className="menu">
-            <div><h1>Heywa Hoodie</h1></div>
+            <h1 className="black">Heywa Hoodie</h1>
+            <div className="precios">
+                <span className="precio2">$30.99</span>
+                <span className="precio3">$44.99</span>
+            </div>
+            <span className="black">Color</span>
             <div className="color">
                 <div className="whiteCircle"></div>
                 <div className="grayCircle"></div>
                 <div className="blackCircle"></div>
                 <div className="blueCircle"></div>
             </div>
+            <span className="black">Size</span>
             <div className="barra">
                 <div className="boton">S</div>
                 <div className="boton">M</div>
